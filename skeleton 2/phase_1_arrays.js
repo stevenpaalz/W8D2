2097a70f796384f46eba51e02b2582cbe809c13f
Justin <jcdiner@gmail.com>
2023-02-28 15:03:00 -0500

    transpose

--- skeleton 2/phase_1_arrays.js
+++ skeleton 2/phase_1_arrays.js
@@ -1,15 +1,43 @@
-// Array.prototype.uniq = function() {
-//     const answer = [];
-//     this.forEach((el)=> {
-//         if (!answer.includes(el)) { answer.push(el);}
-//     })
-//     return answer;
-// }
+ Array.prototype.uniq = function() {
+     const answer = [];
+     this.forEach((el)=> {
+         if (!answer.includes(el)) { answer.push(el);}
+     })
+     return answer;
+ }
 
-Array.prototype.uniq = () => {
-    const answer = [];
-    this.forEach((el) => {
-        if (!answer.includes(el)) { answer.push(el);}
-    })
-    return answer;
+//Array.prototype.uniq = () => {
+//    const answer = [];
+//    this.forEach((el) => {
+//        if (!answer.includes(el)) { answer.push(el);}
+//    })
+//    return answer;
+//}
+
+Array.prototype.twoSum = function() {
+	const answer = [];
+
+	this.forEach((ele, i) => {
+		this.forEach((ele2, i2) => {
+			if (ele + ele2 === 0 && i2 > i) {
+				answer.push([i, i2]);
+			}
+		})
+	})
+	return answer;
 }
+
+Array.prototype.transpose = function() {
+	const answer = new Array(this[0].length).fill([]);
+
+	this.forEach((subArr, idx1) => {
+		subArr.forEach((ele, idx2) => {
+			answer[idx2][idx1] = ele;
+		})
+	})
+	return answer;
+}
+
+const a = [[1, 2, 3],[4, 5, 6]];
+
+console.log(a.transpose())
